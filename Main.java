d9a82aeb51dddbe6132e4b41300aa878ac4fc6f1
FRANTZITHEODORA <99319038+FRANTZITHEODORA@users.noreply.github.com>
2022-04-28 18:29:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+	//ÓÅ ĞÏÉÁ ÔÁÎÇ ĞÑÅĞÅÉ ÍÁ ĞÁÅÉ Ï ÌÁÈÇÔÇÓ
+
+	//ÁÍÏÉÃÙ ÊÁÉÍÏÕÑÃÉÏ PROGECT, ÖÔÉÁ×ÍÙ Main ÊËÁÓÇ
+	//COPY PASTE TEST4, ÄÏÕËÅÕÅÉ
+	
+	public static void main(String[] args) {
+	int age;
+	age = 5;
+	
+	if(age >=4 && age<=5) {
+		System.out.println("Íçğéáãùãåßï");
+	}
+	else if(age >=6 && age<=11) {
+		System.out.println("Äçìïôéêü");
+	}
+	else if(age >=6 && age<=11) {
+		System.out.println("ÃõìíÜóéï");
+	}
+
+	}
+}
